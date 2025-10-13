558d3975bdf1955b6bd00ff4ae3de81d9c6c6939
sknetworks20250226 <gobokuku82@gmail.com>
2025-10-13 16:06:35 +0900

    Fix_Config_settings
    1. 변경된 ML models경로 반영
    2. 환경변수 로드방식 변경

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,18 +1,19 @@
-import os
 from typing import List
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "HolmesNyangz"
-    SECRET_KEY: str = os.getenv("SECRET_KEY","")
+    SECRET_KEY: str = ""
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "")
+    DATABASE_URL: str = ""
     ALLOWED_HOSTS: List[str] = ["http://localhost:3000", "http://127.0.0.1:3000"]
-    MONGODB_URL :str = os.getenv("MONGODB_URL", "")
+    MONGODB_URL: str = ""
     real_estate_data_path: str = "frontend/public/data/real_estate_with_coordinates_kakao.csv"
+
     class Config:
         env_file = ".env"
+        env_file_encoding = "utf-8"
         extra = "allow"  # Allow extra fields from .env file
 
 settings = Settings()
